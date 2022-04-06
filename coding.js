6718de7cefc2db1d02fbe04bc2dbce50c242afc9
Mdiftekhar9123 <md.iftekhar@successive.tech>
2022-04-06 22:26:36 +0530

    Some practice coading solution

---
+++ coding.js
@@ -0,0 +1,190 @@
+// Q:1 wap to print substring to others 
+
+// var s1 = 'for';
+// var s2 = 'geeksforgeeks';
+
+// var found = false;
+// for(let i=0;i<s2.length;i++) {
+//     if(s1[0]==s2[i]) {
+//         var sub = s2.substr(i, s1.length);
+//         if(sub==s1) {
+//             found = true;
+//             break;
+//         }
+//     }
+// }
+
+// if(found) {
+//     console.log("yes", s1, "is present in ", s2);
+// } else {
+//     console.log('no');
+// }
+
+
+// Ques: 2 WAP to print a string without any extra spaces without using any functions.
+
+// For eg: he__llo w____orl_d should be printed as he_llo_w_orl_d
+
+// var chr = "h__e__l lo w____orl_d";
+// var str ="";
+// for(var i=0; i<chr.length; i++) {
+//     if(chr[i] == "_" && chr[i] == chr[i-1]) {
+//         continue;
+//     }
+//      if(chr[i] == " ") {
+//          str += "_";
+//      }else {
+//          str += chr[i]
+//      }
+// }
+// console.log(str);
+
+
+// Ques :-3 wap to print factioral no 
+
+// var n = 5;
+// var i,fact=1;
+
+// if(n<0) {
+//     console.log("inter the positive no");
+//  } else {
+//      for(i=1;i<=n; i++) {
+//          fact = fact*i;
+//      }
+//  }
+//  console.log(fact);
+
+
+// function fact (n) {
+//     if(n == 0 || n==1) {
+//         return 1;
+//     } else {
+//         return n*fact(n-1);
+//     }
+// }
+// let n =5;
+// let ans = fact(n);
+// console.log(ans);
+
+// Ques :4  wap to print fibonacci series ;
+
+// var n1=0,n2 = 1, next;
+// var num =5;
+// for(var i=1; i<=num; i++) {
+//     console.log(n1);
+//     next = n1+n2;
+//     n1 = n2;
+//     n2 = next
+// }
+// console.log(n1);
+
+// Ques : 5 wap to print fabonacci series using recursion;
+
+// function fab(n) {
+//     if(n<2) {
+//         return n;
+//     }
+//     else {
+//         return fab(n-1) + fab(n-2);
+//     }
+// }
+// let n =5;
+// if(n<=0) {
+//     console.log("posotive no");
+// }
+// else {
+//     for(var i=0; i<n; i++) {
+//         console.log(fab(i));
+//     }
+// }
+
+// Ques : 6 wap to remove extra spaces.
+
+// var str = " Hello Geeks . welcome  to .  "
+// out =Hello Geeks.welcome to."
+
+// var str1 = "";
+
+// for(var i =0; i<str.length; i++) {
+
+//     if(i==0 && str[i]==" ") {
+//         continue;
+//     }
+//     if((str[i]==" " && str[i+1]==" ") || (str[i]==" " && str[i+1]==".")) {
+//         continue;
+//     }
+
+//     if(str[i]=="." && str[i+1]==" ") {
+//         str1 += ".";
+//         i++;
+//     } else {
+//         str1 += str[i];
+//     }
+
+// }
+// console.log(str1);
+
+
+
+// Ques :- 7 wap to print hollow triangle pattern;
+
+
+// var start = 1;
+// var space = 5;
+// var out = '';
+// for(let i=0;i<6;i++) {
+//     for(let j=0;j<space;j++) {
+//         out += " ";
+//     }
+
+//     for(let j=1;j<i+1;j++) {
+//         if(i==5) {
+//             out += " "+j;
+//         } else {
+//             if(j>1 && j<start-1) {
+//                 out += "  ";
+//             } else {
+//                 out += " " + j;
+//             }
+//         }
+//     }
+//     out += "\n";
+//     space--;
+//     start++;
+// }
+// console.log(out);
+
+
+// Ques :- 8 wap to print square hollow patterns;
+// let  n =5;
+// let str = "";
+// for(let i =1; i<=n; i++) {
+//     for(let j = 1; j<=n; j++) {
+//         //    if(j==1 || j==5) {
+//             if(i == 1|| j ==1 || i ==n || j ==n ) {
+//                str += j + "  ";
+//             } else {
+//                 str += "   ";
+//             }
+//         }
+//     str += "\n";
+// }
+// console.log(str);
+
+
+// Ques :- 9 wap to print triangle hollow patterns 
+
+let n =5;
+let str = "";
+
+for(let i =1; i<=n; i++) {
+    for(let j =1; j<=i; j++) {
+        if( i == n || j == i || j == 1)  {
+         str += j+ " "
+    } else {
+        str += "  "
+    }
+}
+    str += "\n";
+}
+console.log(str);
---
+++ practice.js
@@ -0,0 +1,406 @@
+// Q1. To print the following pattern
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+// let n= 5;
+// let str = "";
+// for(let i=1; i<=5; i++) {
+//     for(let j=1; j<=i; j++) {
+//         str+= j;   
+//     }
+//      str+="\n";
+// }
+// console.log(str);
+
+// Ques :2 to print diamond patterns
+
+//  const size =5;
+//     let space = "";
+//     for ( let i = 1; i <= size; i++ ) {
+//     for ( let j = size; j > i; j-- ) {
+//     diamond += " ";
+//     }
+//     for ( let k = 0; k < i; k++ ) {
+//     diamond += "* ";
+//     }
+//     diamond += "\n";
+//     }
+//     for ( let i = 1; i <= size - 1; i++ ) {
+//     for ( let j = 0; j < i; j++ ) {
+//     diamond += " ";
+//     }
+//     for ( let k = (size - i); k > 0; k-- ) {
+//     diamond += "* ";
+//     }
+//     diamond += "\n";
+//     }
+//     console.log(diamond);
+
+// Ques :- 3 Reverse of the triangle;
+
+// var n = 5;
+// var str = ""
+// for(let i=n; i>=0; i--) {
+//     for(let j=1; j<=i; j++) {
+//         str+= "*";
+//     }
+//     str+= "\n";
+// }
+// console.log(str);
+
+
+// Ques :- 4 wap to print 3rd largest no in the array ?
+
+// var a = [12,34,62,-71,2,3];
+// var len = a.length;
+// var f = a[0], s = -1000000000, t = -1000000000;
+
+// for(let i = 1; i <len; i++) {
+//     if(a[i]>f) {
+//         t = s;
+//         s = f;
+//         f = a[i];
+//     }
+//     else if (a[i]>s) {
+//         t = s;
+//         s = a[i];
+//     }
+//     else if(a[i]>t) {
+//         t = a[i]    
+//     }
+// }
+// console.log("third largest no is", t);
+
+
+// Ques :-5 wap to check prime no or not ? 
+
+// var n = 2, count = 0;;
+// for(let i=2; i<=n; i++) {
+//     if(n%i==0) {
+//         count++;
+//     }
+
+// }
+// if(count == 2) {
+//     console.log('pm');
+// }
+// else{
+//     console.log('np',);
+// }
+
+// Ques:-6 wap to check no of even and no of odd given the positive no? 
+
+// var n = 7;
+// var e_count =0, o_count=0;
+// for(let i=1; i<=n; i++) {
+//     if(i%2 == 0) {
+//         e_count++;
+//     }
+//     else{
+//         o_count++
+//     }
+// }
+
+// console.log('the total even no is', e_count);
+// console.log('the total odd no is', o_count);
+
+
+// Ques :- 6 reverse traingle patterns
+
+// var n = 5, space = 0,star = 5;
+// var str = "";
+// for(let i=0; i<n; i++) {
+//     for(let j=0;j<space;j++) {
+//         str+= " ";
+//     }
+//     for(let k=0;k<star; k++) {
+//         str+= "*";
+//     }
+//     str+="\n"
+//     space++;
+//     star--;
+// }
+// console.log(str);
+
+
+// Ques :-7 to print full tringle with different no 
+
+// var arr = [2,3,4,1,0,5];
+// var str = "";
+// var max = -1;
+// for(var i=0; i <arr.length; i++) {
+//     // max = max(arr[i], max);
+
+//     if(max<arr[i]) {
+//         max = arr[i];
+//     }
+//     str += arr[i];
+// }
+// str += "\n";
+
+// for(var i=0;i<max;i++) {
+//     for(var j=0;j<arr.length;j++) {
+//         if(arr[j]<=0) {
+//             str += " ";
+//         } else {
+
+//             str += "*";
+//         }
+//         arr[j]--;
+//     }
+//     str += "\n";
+// }
+// console.log(str);
+
+
+// Ques:-8  given the some random no in the array print the star according to no: 
+
+
+// var arr = [3,4,1,0,7];
+// var str = "";
+// var max = -1;
+
+// for(var i=0; i<arr.length;i++) {
+//     if(arr[i]>max) {
+//         max = arr[i];
+//     }
+//     str+= arr[i];
+// }
+// str+= '\n'
+// for(var i=0; i<max; i++) {
+//     for(var j=0; j<arr.length;j++) {
+//         if(arr[j]<=0) {
+//             str+= " ";
+//         }
+//         else{
+//             str+= "*";
+//         }
+//         arr[j]--;
+//     }
+//     str+='\n';
+// }
+// console.log(str);
+
+// Ques : 9 print opposite traingle star 
+
+// var n = 4, space = 3, star =1;
+// let str = "";
+// for(var i=0; i<n; i++) {
+//     for(var j=0; j<space; j++) {
+//         str+= " ";
+//     }
+//     for(var k =0; k<star; k++) {
+//         str+= "*";
+//     }
+//     str+= "\n"
+//     star++;
+//     space--;
+// }
+// console.log(str);
+
+
+// Ques:- 10 to print full pyramid diamond start   
+
+// var n = 4, space = 3, star =4,max =4;
+// let str = "";
+// for(var i=0; i<n; i++) {
+//     for(var j=0; j<space; j++) {
+//         str+= " ";
+//     }
+//     for(var j = star; j<max; j++) {
+//         str+= "*";
+//     }
+//     for(var j=max; j>=star; j--){
+//         str+="*";
+//     }
+//     if(i<3) {
+//         str+= "\n"
+//     }
+
+//     star--;
+//     space--;
+// }
+// console.log(str);
+// n =4 , space =0, star = 4; 
+// str= "";
+
+// for(var i=0; i<n; i++) {
+//     for(var j=0; j<space; j++) {
+//         str+=" ";
+//     }
+//     for(var k = 0; k<star; k++) {
+//          str+= "*";
+//     }
+//     for(var k=star; k>1; k--) {
+//         str+="*";
+//     }
+
+//     str+="\n";
+//     star--;
+//     space++;
+// }
+// console.log(str);
+
+
+
+// Ques:11 wap to print traingle with different no
+
+// var n = 9, max_val = 1, start_val = 1 , space = 8;
+// var str = "";
+// var space = 8;
+// var cur_val = 1;
+// for(var i=0;i<9; i++) { 
+//     for(var j=0;j<space;j++) {
+//         str += " ";
+//     }
+//     for(var j=1;j<i+1;j++) {
+//         str += j;
+//     }
+//     for(var j=i+1;j>=1;j--) {
+//         str += j;
+//     }
+//     str += "\n";
+//     cur_val++;
+//     space--;
+// }
+// console.log(str);
+
+
+
+
+
+// Ques: 12 Write a program to reverse the string?
+
+
+// var chr= "HELLO WORLD";
+// var str = "";
+
+// var st = chr.split(" ");
+// console.log(st);
+// for(var i=st.length-1;i>=0;i--) {
+//     str += st[i];
+//     str += " ";
+// }
+// console.log(str);
+
+
+// Ques:- 13  delete the second position element in the array. ?
+
+// var a = [12,34,62,-71,2,3];
+// var len = a.length;
+// var f = a[0], s = -1000000000;
+
+// for(let i = 1; i <len; i++) {
+//     if(a[i]>f) {
+//         s = f;
+//         f = a[i];
+//     }
+//     else if (a[i]>s) {
+//         s = a[i];
+//     }
+// }
+
+// console.log("second largest no is", s);
+
+
+// Ques:-14 Program to reverse a number and print its sum
+
+// var a = [3,4,5,6,7];
+// var sum = 0, str= "";
+
+// for( var i=a.length-1; i>=0; i--) {
+//     sum += a[i] ;      
+// }
+// console.log(sum);
+
+
+//Ques:- 15 find the program of palindrom number
+
+// var n = 151;
+// var temp = n, sum =0, rem = 0;
+
+// while(n>0) {
+//     rem = n % 10;
+//     console.log('rem,', rem);
+//     sum = sum*10+rem;
+//     n = n/10;
+// }
+// console.log(sum);
+// if(temp == sum) {
+//     console.log("no is palindrome");
+// }
+// else {
+//     console.log(" no  is not palindrome");
+// }
+
+
+
+//Ques:- 16 check if string is substring of other string using inbuilt functions ?
+
+
+// var s1 = 'for';
+// var s2 = 'geeksforgeeks';
+
+// if(s2.includes(s1)) {
+//     console.log("s1 is presert is s2");
+// } else {
+//     console.log("s1 is not present in s2");
+// }
+
+
+//Ques:- 17 check if string is substring of other string without using inbuilt functions ?
+
+// var s1 = 'for';
+// var s2 = 'geeksforgeeks';
+// var found = false;
+// for(let i=0;i<s2.length;i++) {
+//     if(s1[0]==s2[i]) {
+//         var sub = s2.substr(i, s1.length);
+//         if(sub==s1) {
+//             found = true;
+//             break;
+//         }
+//     }
+// }
+// if(found) {
+//     console.log("yes", s1, "is present in ", s2);
+// } else {
+//     console.log('no');
+// }
+
+
+//Ques:- 17 check if string is substring of other string and 
+// only matched firs and last character ?
+
+
+// var s1 = 'far';
+// var s2 = 'geeksforgeeks';
+// var found = false;
+
+// for(let i=0;i<s2.length;i++) {
+//     if(s1[0]==s2[i] && s1[s1.length-1]==s2[i+s1.length-1]) {
+//         found = true;
+//         break;
+//     }
+// }
+
+// if(found) {
+//     console.log("yes", s1, "is present in ", s2);
+// } else {
+//     console.log('no');
+// }
+
+
+function josephus(n, k) {
+        if (n == 1) 
+                return 1;
+        else 
+                return (josephus(n - 1,k) + (k - 1)) % (n + 1);
+       
+}
+let n = 8;
+let k = 2;
+console.log("the chosen place of josephus:" , josephus(n, k));

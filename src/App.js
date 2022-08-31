57b90f931d5f8c2982ff0367ecdfcde178ce9363
Mohammad Iftekhar <md.iftekhar@iftekharr401.successivetech.com>
2022-08-31 12:16:05 +0530

    Making a computer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
+import { jsPDF } from "jspdf";
+import calu from '../src/Image/c.png';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState('');
+
+  const clickHandler = (e) => {
+    setResult(result.concat(e.target.value));
+  }
+  const clearHandler = () => {
+    setResult(''); 
+  }
+  const calculate = () => {
+    setResult(eval(result).toString());
+  }
+  const pdfGenerate = () => {
+    var doc = new jsPDF('landscape','px','a4','false');
+    // doc.text('Hello world',10,10);
+    doc.addImage(calu,'PNG',65,20,500,400);
+    doc.save('c.pdf')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <div className="calc">
+        <input type="text" placeholder="0" id="answer" value={result}/>
+        <input type="button" value="9" className="button" onClick={clickHandler}/>
+        <input type="button" value="8" className="button" onClick={clickHandler}/>
+        <input type="button" value="7" className="button" onClick={clickHandler}/>
+        <input type="button" value="6" className="button" onClick={clickHandler}/>
+        <input type="button" value="5" className="button" onClick={clickHandler}/>
+        <input type="button" value="4" className="button" onClick={clickHandler}/>
+        <input type="button" value="3" className="button" onClick={clickHandler}/>
+        <input type="button" value="2" className="button" onClick={clickHandler}/>
+        <input type="button" value="1" className="button" onClick={clickHandler}/>
+        <input type="button" value="0" className="button" onClick={clickHandler}/>
+        <input type="button" value="." className="button" onClick={clickHandler}/>
+        <input type="button" value="+" className="button" onClick={clickHandler}/>
+        <input type="button" value="-" className="button" onClick={clickHandler}/>
+        <input type="button" value="*" className="button" onClick={clickHandler}/>
+        <input type="button" value="/" className="button" onClick={clickHandler}/>
+        <input type="button" value="%" className="button" onClick={clickHandler}/>
+        <input type="button" value="clear" className=" button1" onClick={clearHandler}/>
+        <input type="button" value="=" className=" button1" onClick={calculate}/>
+        
+        
+        <button style={{marginTop:"40px", padding : "10px", fontSize : "30px",
+        backgroundColor :"yellow", cursor:"pointer"}} onClick = {pdfGenerate}> download pdf</button>
+
+
+      </div> 
+      <div>
+          
+      </div> 
     </div>
   );
 }
